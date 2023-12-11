609dbf6333533033a229a65441c4a351b37b6a34
Prthmesh6 <prathmeshpatilp@gmail.com>
2023-12-11 21:24:55 +0530

    Adding Websocket code to gitHub

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	// Create a root ctx and a CancelFunc which can be used to cancel retentionMap goroutine
+	rootCtx := context.Background()
+	ctx, cancel := context.WithCancel(rootCtx)
+
+	defer cancel()
+
+	setupAPI(ctx)
+
+	// Serve on port :8080, fudge yeah hardcoded port
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+
+}
+
+// setupAPI will start all Routes and their Handlers
+func setupAPI(ctx context.Context) {
+
+	// Create a Manager instance used to handle WebSocket Connections
+	manager := NewManager(ctx)
+
+	// Serve the ./frontend directory at Route /
+	http.Handle("/", http.FileServer(http.Dir("./frontend")))
+	http.HandleFunc("/login", manager.loginHandler)
+	http.HandleFunc("/ws", manager.serveWS)
+
+	http.HandleFunc("/debug", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, len(manager.clients))
+	})
+}
